3b65cc747fa25d376f5d8ba3265cfaa600f19001
shuntaro4 <shuntaro4@gmail.com>
2019-07-30 21:24:19 +0900

    remove BindableBase class.

--- WebApplication/ViewModels/SampleViewModel.cs
+++ WebApplication/ViewModels/SampleViewModel.cs
@@ -1,30 +1,10 @@
 ﻿namespace WebApplication.ViewModels
 {
-    public class SampleViewModel : BindableBase
+    public class SampleViewModel
     {
-        private int _value1;
+        public int Value1 { get; set; }
 
-        public int Value1
-        {
-            get => _value1;
-            set
-            {
-                SetProperty(ref _value1, value);
-                OnPropertyChanged(nameof(Result));
-            }
-        }
-
-        private int _value2;
-
-        public int Value2
-        {
-            get => _value2;
-            set
-            {
-                SetProperty(ref _value2, value);
-                OnPropertyChanged(nameof(Result));
-            }
-        }
+        public int Value2 { get; set; }
 
         public int Result => Value1 + Value2;
     }
